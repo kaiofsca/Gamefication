67a6b31d0c9001a068593b46105b4bac5add2e9b
kaiofsca <mkaiofonseca@gmail.com>
2024-06-11 17:18:06 -0300

    Projeto v1

--- src/components/store/Store.tsx
+++ src/components/store/Store.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Modal from "react-modal";
 import { Link } from "react-router-dom";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
